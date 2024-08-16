e4d1e195bd6381b1c670fb38d39419343b89491e
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-08-15 23:49:51 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['phantom-marca.unidadeditorial.es'],
+  },
+};
 
 export default nextConfig;
